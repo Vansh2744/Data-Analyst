59617e5c06f21eabb72a8a544a1d889f0e766867
Vansh <vanshchoudhary2744@gmail.com>
2026-09-24 20:29:01 +0530

    mutable types

---
+++ mutable.py
@@ -0,0 +1,27 @@
+# names = set(['Vansh','Aman','Rahul'])
+
+# print(id(names))
+
+# names.add('Kartik')
+
+# print(id(names))
+
+#---------------------
+
+# names = ['Vansh','Rahul','Kartik']
+
+# print(id(names))
+
+# names.append('Aman')
+
+# print(id(names))
+
+#----------------------
+
+user = {'name':'Vansh', 'email':'vansh@gmail.com', 'age':23}
+
+print(id(user))
+
+user['is_subscribed'] = True
+
+print(id(user))
