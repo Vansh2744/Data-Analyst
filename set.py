88df57a16eec0f61a8830d162516f26b85e6f683
Vansh <vanshchoudhary2744@gmail.com>
2026-09-25 10:30:35 +0530

    set

---
+++ set.py
@@ -0,0 +1,7 @@
+names1 = {'Vansh','Aman','Kartik'}
+names2 = {'Vansh','Rahul','Rohan','Megha','Aman'}
+
+print(names1 | names2) #union
+print(names1 & names2) #intersection
+print(names1 - names2)
+print(names2 - names1)
