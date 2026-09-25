5c7b76f479dae0df3b072d071ff2beeb98e0ef78
Vansh <vanshchoudhary2744@gmail.com>
2026-09-25 06:22:19 +0530

    list

---
+++ list.py
@@ -0,0 +1,53 @@
+# names = ['Vansh','Aman','Rahul']
+
+# names.reverse()
+
+# print(names)
+
+# names1 = ['Kartik','Rohan']
+
+# names.extend(names1)
+
+# print(names)
+
+# names += names1
+
+# print(names)
+
+# names.insert(2,"Rohan")
+
+# print(names)
+
+# names[0] = "Kartik"
+
+# print(names)
+
+# names.append("Rohan")
+
+# print(names.pop())
+
+# print(names)
+
+# names.sort()
+# print(names)
+
+# names.sort(reverse=True)
+
+# print(names)
+
+nums = [23,45,67,12,12]
+
+print(max(nums))
+print(min(nums))
+
+print(nums.count(12))
+
+print(nums.index(67))
+
+nums.remove(12)
+
+print(nums)
+
+nums.clear()
+
+print(nums)
