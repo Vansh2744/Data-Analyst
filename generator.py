a43af93d321dcf5f1343780647c5eefbeca97f3d
Vansh <vanshchoudhary2744@gmail.com>
2026-09-27 02:39:27 +0530

    infinite generator

--- generator.py
+++ generator.py
@@ -1,15 +1,36 @@
-def run_tasks():
-    yield "Task 1"
-    yield "Task 2"
-    yield "Task 3"
-    yield "Task 4"
+# def run_tasks():
+#     yield "Task 1"
+#     yield "Task 2"
+#     yield "Task 3"
+#     yield "Task 4"
 
-res = run_tasks()
+# res = run_tasks()
 
-# print(next(res))
-# print(next(res))
-# print(next(res))
-# print(next(res))
+# # print(next(res))
+# # print(next(res))
+# # print(next(res))
+# # print(next(res))
 
-for task in res:
-    print(task)
+# for task in res:
+#     print(task)
+
+
+#----------Infinite Generator-------------
+
+def infinite_gen():
+    count = 1
+    while True:
+        yield f"Count : {count}"
+        count += 1
+
+loop_gen = infinite_gen()
+loop_gen1 = infinite_gen()
+
+for _ in range(5):
+    print(next(loop_gen))
+
+for _ in range(5):
+    print(next(loop_gen))
+
+for _ in range(5):
+    print(next(loop_gen1))
