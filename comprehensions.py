982c9b6cffc3779c86fe6bd641bc70c8b96ad1b4
Vansh <vanshchoudhary2744@gmail.com>
2026-09-27 01:30:54 +0530

    dictionary comprehensive

--- comprehensions.py
+++ comprehensions.py
@@ -14,8 +14,45 @@
 
 #-------------Set Comprehension------------------
 
-names = ["Vansh","Aman","Rahul","Kartik","Rahul","Rahul"]
+# names = ["Vansh","Aman","Rahul","Kartik","Rahul","Rahul"]
 
-res_names = {name for name in names}
+# res_names = {name for name in names}
 
-print(res_names)
+# print(res_names)
+
+#--------------Dictionary Comprehension--------------
+
+# users = {"Vansh":67,"Aman":90, "Rahul":45, "Kartik":34}
+
+# # res_marks = {name:users[name]+10 for name in users if users[name] < 91}
+
+# res_marks = {key:val+10 for key,val in users.items() if val < 91}
+
+# print(res_marks)
+
+
+# users = [{
+#     'name':"Vansh",
+#     'email':"vansh@gmail.com",
+#     'age':23
+# },{
+#     'name':"Aman",
+#     'email':"aman@gmail.com",
+#     'age':23
+# },{
+#     'name':"Kartik",
+#     'email':"kartik@gmail.com",
+#     'age':23
+# },{
+#     'name':"Rahul",
+#     'email':"rahul@gmail.com",
+#     'age':23
+# }]
+
+
+# res_user = {key:val for user in users if user['email'] == "vansh@gmail.com" for key, val in user.items()}
+
+# print(res_user)
+
+#-----------Generator Comprehension----------
+
