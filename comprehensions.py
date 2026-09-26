16a075febd624717c398ebec900cd4c5edd47b0b
Vansh <vanshchoudhary2744@gmail.com>
2026-09-27 01:39:39 +0530

    generator comprehension

--- comprehensions.py
+++ comprehensions.py
@@ -56,3 +56,13 @@
 
 #-----------Generator Comprehension----------
 
+# scores = [12,34,56,78,90,100]
+
+# res = (score for score in scores if score > 50)
+
+# # print(next(res))
+# # print(next(res))
+# # print(next(res))
+# # print(next(res))
+
+# print(list(res))
