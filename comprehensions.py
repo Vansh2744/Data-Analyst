4a63b80e0e08fdd3741084312d025c2b63bc4b9b
Vansh <vanshchoudhary2744@gmail.com>
2026-09-27 01:20:32 +0530

    set comprehensive

--- comprehensions.py
+++ comprehensions.py
@@ -1,13 +1,21 @@
 #-----------List Comprehension-------------
 
-names = ["Vansh", "Aman", "Rahul", "Rohan", "Kartik", "Ritik"]
+# names = ["Vansh", "Aman", "Rahul", "Rohan", "Kartik", "Ritik"]
 
-res = [name for name in names if name[0].lower() == 'r']
+# res = [name for name in names if name[0].lower() == 'r']
 
-print(res)
+# print(res)
 
-scores = [10, 50, 60, 95, 54]
+# scores = [10, 50, 60, 95, 54]
 
-updated_scores = [score+10 for score in scores if not(score > 90)]
+# updated_scores = [score+10 for score in scores if not(score > 90)]
 
-print(updated_scores)
+# print(updated_scores)
+
+#-------------Set Comprehension------------------
+
+names = ["Vansh","Aman","Rahul","Kartik","Rahul","Rahul"]
+
+res_names = {name for name in names}
+
+print(res_names)
